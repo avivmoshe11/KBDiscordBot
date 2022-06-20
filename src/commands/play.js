bfb3093e9af17cfceffa78a663b584d974210da3
AvivMoshe11 <avivmoshe123@gmail.com>
2022-06-20 22:38:10 +0300

    Fully Functional bot

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,9 +1,9 @@
 const ytdl = require("ytdl-core");
 const ytSearch = require("yt-search");
 const prefix = require("../bot");
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, entersState } = require("@discordjs/voice");
 const queue = new Map();
-
+let player;
 module.exports.run = async (client, msg, args) => {
   const voice_channel = msg.member.voice.channel;
   if (!voice_channel) return msg.channel.send("you must be in a channel to execute this command!");
@@ -12,9 +12,7 @@ module.exports.run = async (client, msg, args) => {
   if (!permissions.has("CONNECT")) return msg.channel.send("i don't have permissions to join your channel");
   if (!permissions.has("SPEAK")) return msg.channel.send("i don't have permissions to sing in your channel");
 
-  console.log("play was activated");
-
-  const server_queue = queue.get(msg.guild.id);
+  let server_queue = queue.get(msg.guild.id);
 
   let commandName = msg.content.slice(prefix.modules.prefix.length).trim().split(/ +/g).shift().toLowerCase();
   if (commandName == "play") {
@@ -48,7 +46,7 @@ module.exports.run = async (client, msg, args) => {
         connection: null,
         songs: [],
       };
-
+      player = createAudioPlayer();
       queue.set(msg.guild.id, queue_constructor);
       queue_constructor.songs.push(song);
       try {
@@ -59,7 +57,7 @@ module.exports.run = async (client, msg, args) => {
         });
 
         queue_constructor.connection = connection;
-        video_player(msg.guild, queue_constructor.songs[0]);
+        video_player(msg.guild, queue_constructor.songs[0], player);
       } catch (err) {
         queue.delete(msg.guild.id);
         msg.channel.send("error connecting");
@@ -70,27 +68,72 @@ module.exports.run = async (client, msg, args) => {
       return msg.channel.send(`👍 **${song.title} added to queue!`);
     }
   }
+  if (commandName == "pause") {
+    if (!player) return msg.channel.send("bot is not in a voice channel");
+    if (!msg.member.voice.channel) return msg.channel.send("You need to be in a channel to execute this command!");
+    if (msg.member.voice.channel.id != server_queue.connection.joinConfig.channelId)
+      return msg.channel.send("you must be in the same channel as the bot");
+    if (player._state.status == "playing") {
+      player.pause();
+      msg.channel.send("👌 paused!");
+    }
+  }
+  if (commandName == "unpause") {
+    if (!player) return msg.channel.send("bot is not in a voice channel");
+    if (!msg.member.voice.channel) return msg.channel.send("You need to be in a channel to execute this command!");
+    if (msg.member.voice.channel.id != server_queue.connection.joinConfig.channelId)
+      return msg.channel.send("you must be in the same channel as the bot");
+
+    if (player._state.status == "paused") {
+      player.unpause();
+      msg.channel.send("👌 unpaused!");
+    }
+  }
+  if (commandName == "leave") {
+    if (!player) return msg.channel.send("bot is not in a voice channel");
+    if (!server_queue) {
+      return msg.channel.send(`💤 already sleeping... `);
+    }
+    if (!msg.member.voice.channel) return msg.channel.send("You need to be in a channel to execute this command!");
+    if (msg.member.voice.channel.id != server_queue.connection.joinConfig.channelId)
+      return msg.channel.send("you must be in the same channel as the bot");
+    server_queue.connection.destroy();
+    server_queue = null;
+    queue.delete(msg.guild.id);
+    player = null;
+    msg.channel.send("💔 cya soon");
+  }
+  if (commandName == "skip") {
+    if (!player) return msg.channel.send("bot is not in a voice channel");
+    if (!server_queue) {
+      return msg.channel.send(`There are no songs in queue 😔`);
+    }
+    if (!msg.member.voice.channel) return msg.channel.send("You need to be in a channel to execute this command!");
+    if (msg.member.voice.channel.id != server_queue.connection.joinConfig.channelId)
+      return msg.channel.send("you must be in the same channel as the bot");
+    server_queue.songs.shift();
+    msg.channel.send("👌 skipped!");
+    video_player(msg.guild, server_queue.songs[0], player);
+  }
 };
 
-const video_player = async (guild, song) => {
-  const song_queue = queue.get(guild.id);
+const video_player = async (guild, song, player) => {
+  let song_queue = queue.get(guild.id);
 
   if (!song) {
-    //song_queue.voice_channel.leave();
+    song_queue.connection.destroy();
+    song_queue = null;
     queue.delete(guild.id);
     return;
   }
-  const player = createAudioPlayer();
   const stream = ytdl(song.url, { filter: "audioonly" });
   let resource = createAudioResource(stream);
-  console.log(typeof song_queue, song_queue);
+  //console.log(typeof song_queue, song_queue);
   song_queue.connection.subscribe(player);
-  player.addListener("stateChange", (oldOne, newOne) => {
-    if (newOne.status == "idle") {
-      console.log("The song finished");
-      song_queue.songs.shift();
-      video_player(guild, song_queue.songs[0]);
-    }
+
+  player.on(AudioPlayerStatus.Idle, () => {
+    song_queue.songs.shift();
+    video_player(guild, song_queue.songs[0], player);
   });
 
   await song_queue.text_channel.send(`🎶 now playing **${song.title}**`);
@@ -100,7 +143,7 @@ const video_player = async (guild, song) => {
 module.exports.help = {
   name: "play",
   description: "makes the bot sing",
-  aliases: ["p", "skip", "stop"],
+  aliases: ["p", "skip", "stop", "pause", "unpause", "leave"],
 };
 // .on("finish", () => {
 //   song_queue.songs.shift();
